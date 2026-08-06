c481a233ae226556593b1d815932c1935e2d2902
omkrishna24mim10181-cmyk <omkrishna.24mim10181@vitbhopal.ac.in>
2026-08-06 21:15:07 +0530

    Create Stones_on_the_Table.java

---
+++ Stones_on_the_Table.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Stones_on_the_Table {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String s = sc.next();
+
+        int count = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+
+            if (s.charAt(i) == s.charAt(i - 1))
+                count++;
+        }
+
+        System.out.println(count);
+    }
+}
