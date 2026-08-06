2da3a39a08aa7e2f3e7c308f0250c1cf833ef1dd
omkrishna24mim10181-cmyk <omkrishna.24mim10181@vitbhopal.ac.in>
2026-08-06 21:10:25 +0530

    Create Beautiful_Matrix.java

---
+++ Beautiful_Matrix.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Beautiful_Matrix {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int row = 0;
+        int col = 0;
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+
+                int x = sc.nextInt();
+
+                if (x == 1) {
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+
+        System.out.println(Math.abs(row - 2) + Math.abs(col - 2));
+    }
+}
