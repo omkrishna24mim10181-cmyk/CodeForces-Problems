219afec28a386cfc6e010026a823a262fa98c44c
omkrishna24mim10181-cmyk <omkrishna.24mim10181@vitbhopal.ac.in>
2026-08-12 10:19:54 +0530

    Create BeautifulYear.java

---
+++ BeautifulYear.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class import java.util.Scanner;
+
+public class BeautifulYear {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        while (true) {
+            year++;
+
+            String s = String.valueOf(year);
+
+            if (s.charAt(0) != s.charAt(1) &&
+                s.charAt(0) != s.charAt(2) &&
+                s.charAt(0) != s.charAt(3) &&
+                s.charAt(1) != s.charAt(2) &&
+                s.charAt(1) != s.charAt(3) &&
+                s.charAt(2) != s.charAt(3)) {
+
+                System.out.println(year);
+                break;
+            }
+        }
+    }
+} {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        while (true) {
+            year++;
+
+            String s = String.valueOf(year);
+
+            if (s.charAt(0) != s.charAt(1) &&
+                s.charAt(0) != s.charAt(2) &&
+                s.charAt(0) != s.charAt(3) &&
+                s.charAt(1) != s.charAt(2) &&
+                s.charAt(1) != s.charAt(3) &&
+                s.charAt(2) != s.charAt(3)) {
+
+                System.out.println(year);
+                break;
+            }
+        }
+    }
+}
